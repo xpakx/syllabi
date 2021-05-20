8c6566db8ce5bff2508d4625888e9a5c6097e7f5
xpax <55586470+xpakx@users.noreply.github.com>
2021-05-20 14:07:22 +0200

    chore: initialization

---
+++ backend/src/main/java/io/github/xpax/syllabi/SyllabiApplication.java
@@ -0,0 +1,13 @@
+package io.github.xpax.syllabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyllabiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyllabiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/xpax/syllabi/SyllabiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xpax.syllabi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyllabiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
