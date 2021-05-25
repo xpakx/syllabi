27e9666f22868015f2212873643d7dabd51e0b5f
xpax <55586470+xpakx@users.noreply.github.com>
2021-05-25 11:24:27 +0200

    feat(frontend): add study group service

---
+++ frontend/src/app/service/study-group.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { StudyGroup } from '../entity/study-group';
+import { StudyGroupRequest } from '../entity/study-group-request';
+import { StudyGroupSummary } from '../entity/study-group-summary';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudyGroupService {
+  private url = environment.apiServerUrl + "/groups";
+
+  constructor(private http: HttpClient) { }
+
+  public getStudyGroupById(id: number): Observable<StudyGroup> {
+    return this.http.get<StudyGroup>(`${this.url}/${id}`);
+  }
+
+  public getStudyGroupByIdMin(id: number): Observable<StudyGroupSummary> {
+    return this.http.get<StudyGroupSummary>(`${this.url}/${id}`);
+  }
+
+  public deleteStudyGroup(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.url}/${id}`);
+  }
+
+  public editStudyGroup(id: number, group: StudyGroupRequest): Observable<StudyGroup> {
+    return this.http.put<StudyGroup>(`${this.url}/${id}`, group);
+  }
+}
