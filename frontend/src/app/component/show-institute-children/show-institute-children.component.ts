e328845e76b7c088acfd14907f6fbbb600cbf61c
xpax <55586470+xpakx@users.noreply.github.com>
2021-05-27 12:54:26 +0200

    refactor(frontend): add institute-children adapter

--- frontend/src/app/component/show-institute-children/show-institute-children.component.ts
+++ frontend/src/app/component/show-institute-children/show-institute-children.component.ts
@@ -2,10 +2,13 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Institute } from 'src/app/entity/institute';
 import { InstituteForPage } from 'src/app/entity/institute-for-page';
 import { Page } from 'src/app/entity/page';
+import { InstituteChildrenAdapterService } from 'src/app/service/institute-children-adapter.service';
 import { InstituteService } from 'src/app/service/institute.service';
 import { ModalDeleteInstituteComponent } from '../modal-delete-institute/modal-delete-institute.component';
+import { PageableGetAllChildrenComponent } from '../pageable/pageable-get-all-children.component';
 import { PageableComponent } from '../pageable/pageable.component';
 
 @Component({
@@ -13,18 +16,20 @@ import { PageableComponent } from '../pageable/pageable.component';
   templateUrl: './show-institute-children.component.html',
   styleUrls: ['./show-institute-children.component.css']
 })
-export class ShowInstituteChildrenComponent extends PageableComponent<InstituteForPage> implements OnInit {
-  
-  constructor(private instituteService: InstituteService, private dialog: MatDialog,
-    private router: Router, private route: ActivatedRoute) { 
-      super();
+export class ShowInstituteChildrenComponent extends PageableGetAllChildrenComponent<InstituteForPage> implements OnInit {
+  institute: Institute | undefined;
+
+  constructor(protected service: InstituteChildrenAdapterService, private dialog: MatDialog,
+    protected router: Router, protected route: ActivatedRoute) { 
+      super(service, router, route);
     }
 
   ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.instituteService.getAllChildren(id).subscribe(
-      (response: Page<InstituteForPage>) => {
-        this.printPage(response);
+    this.getFirstPage();
+
+    this.service.getParentById(this.id).subscribe(
+      (result: Institute) => {
+        this.institute = result;
       },
       (error: HttpErrorResponse) => {
         if(error.status === 401) {
@@ -34,22 +39,6 @@ export class ShowInstituteChildrenComponent extends PageableComponent<InstituteF
         this.message = error.error.message;
       }
     );
-  }
-
-  getPage(page: number): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.instituteService.getAllChildrenForPage(id, page).subscribe(
-      (response: Page<InstituteForPage>) => {
-        this.printPage(response);
-      },
-      (error: HttpErrorResponse) => {
-        if(error.status === 401) {
-          localStorage.removeItem("token");
-          this.router.navigate(['login']);
-        }
-        this.message = error.error.message;
-      }
-    )
   }
   
   delete(id: number, name: string) {
--- frontend/src/app/service/institute-children-adapter.service.ts
+++ frontend/src/app/service/institute-children-adapter.service.ts
@@ -2,25 +2,25 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Institute } from "../entity/institute";
+import { InstituteForPage } from "../entity/institute-for-page";
 import { Page } from "../entity/page";
-import { ProgramForPage } from "../entity/program-for-page";
 import { InstituteService } from "./institute.service";
 import { ServiceWithGetAllChildren } from "./service-with-get-all-children";
 
 @Injectable({
     providedIn: 'root'
 })
-export class InstituteProgramsAdapterService
-implements ServiceWithGetAllChildren<ProgramForPage> {
+export class InstituteChildrenAdapterService
+implements ServiceWithGetAllChildren<InstituteForPage> {
 
     constructor(protected http: HttpClient, private service: InstituteService) {  }
 
-    public getAllByParentId(id: number): Observable<Page<ProgramForPage>> {
-        return this.service.getAllPrograms(id);
+    public getAllByParentId(id: number): Observable<Page<InstituteForPage>> {
+        return this.service.getAllChildren(id);
     }
     
-    public getAllByParentIdForPage(id: number, page: number): Observable<Page<ProgramForPage>> {
-        return this.service.getAllProgramsForPage(id, page);
+    public getAllByParentIdForPage(id: number, page: number): Observable<Page<InstituteForPage>> {
+        return this.service.getAllChildrenForPage(id, page);
     }
 
     public getParentById(id: number): Observable<Institute> {
