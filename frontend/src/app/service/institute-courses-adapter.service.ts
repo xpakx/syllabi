fdba53f3066cd78c78df446ec981b810811cd867
xpax <55586470+xpakx@users.noreply.github.com>
2021-05-27 12:44:17 +0200

    refactor(frontend): add institute-courses adapter

---
+++ frontend/src/app/service/institute-courses-adapter.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { CourseForPage } from "../entity/course-for-page";
+import { Institute } from "../entity/institute";
+import { Page } from "../entity/page";
+import { InstituteService } from "./institute.service";
+import { ServiceWithGetAllChildren } from "./service-with-get-all-children";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class InstituteCoursesAdapterService
+implements ServiceWithGetAllChildren<CourseForPage> {
+
+    constructor(protected http: HttpClient, private service: InstituteService) {  }
+
+    public getAllByParentId(id: number): Observable<Page<CourseForPage>> {
+        return this.service.getAllCourses(id);
+    }
+    
+    public getAllByParentIdForPage(id: number, page: number): Observable<Page<CourseForPage>> {
+        return this.service.getAllCoursesForPage(id, page);
+    }
+
+    public getParentById(id: number): Observable<Institute> {
+        return this.service.getById(id);
+    }
+}
