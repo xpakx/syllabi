44d5445fcb58bdb458d524933f84429a93d8f15d
xpax <55586470+xpakx@users.noreply.github.com>
2021-05-31 11:20:54 +0200

    refactor(frontend): extend show institute with show component

--- frontend/src/app/component/show-institute/show-institute.component.ts
+++ frontend/src/app/component/show-institute/show-institute.component.ts
@@ -1,52 +1,28 @@
-import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Institute } from 'src/app/entity/institute';
 import { InstituteService } from 'src/app/service/institute.service';
+import { UserService } from 'src/app/service/user.service';
 import { ModalDeleteInstituteComponent } from '../modal-delete-institute/modal-delete-institute.component';
+import { ShowComponent } from '../show/show-component.component';
 
 @Component({
   selector: 'app-show-institute',
   templateUrl: './show-institute.component.html',
   styleUrls: ['./show-institute.component.css']
 })
-export class ShowInstituteComponent implements OnInit {
+export class ShowInstituteComponent extends ShowComponent<Institute> implements OnInit {
   institute: Institute | undefined;
-  message: string = '';
 
-  constructor(private instituteService: InstituteService, private route: ActivatedRoute, 
-    private dialog: MatDialog, private router: Router) { }
+  constructor(protected instituteService: InstituteService, protected userService: UserService,
+    protected route: ActivatedRoute, 
+    private dialog: MatDialog, protected router: Router) { 
+      super(instituteService, userService, router, route);
+    }
 
   ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.instituteService.getById(id).subscribe(
-      (result: Institute) => {
-        this.institute = result;
-      },
-      (error: HttpErrorResponse) => {
-        if(error.status === 401) {
-          localStorage.removeItem("token");
-          this.router.navigate(['login']);
-        }
-        this.message = error.error.message;
-      }
-    );
-  }
-
-  loadCourse(id: number): void {
-    this.instituteService.getById(id).subscribe(
-      (result: Institute) => {
-        this.institute = result;
-      },
-      (error: HttpErrorResponse) => {
-        if(error.status === 401) {
-          localStorage.removeItem("token");
-          this.router.navigate(['login']);
-        }
-        this.message = error.error.message;
-      }
-    );
+    this.getElem();
   }
 
   delete(id: number, name: string) {
