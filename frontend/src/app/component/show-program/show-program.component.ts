42ed9ffd2e5f1643c1c9a611a61ff1f4dee0ea39
xpax <55586470+xpakx@users.noreply.github.com>
2021-05-31 11:23:29 +0200

    refactor(frontend): extend show program with show component

--- frontend/src/app/component/show-program/show-program.component.ts
+++ frontend/src/app/component/show-program/show-program.component.ts
@@ -3,52 +3,26 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Program } from 'src/app/entity/program';
-import { ProgramSummary } from 'src/app/entity/program-summary';
 import { ProgramService } from 'src/app/service/program.service';
-import { ModalDeleteStudyGroupComponent } from '../modal-delete-study-group/modal-delete-study-group.component';
+import { UserService } from 'src/app/service/user.service';
 import { ModalProgramDeleteComponent } from '../modal-program-delete/modal-program-delete.component';
+import { ShowComponent } from '../show/show-component.component';
 
 @Component({
   selector: 'app-show-program',
   templateUrl: './show-program.component.html',
   styleUrls: ['./show-program.component.css']
 })
-export class ShowProgramComponent implements OnInit {
-  program: Program | undefined;
-  message: string = '';
+export class ShowProgramComponent extends ShowComponent<Program> implements OnInit {
 
-  constructor(private programService: ProgramService, private route: ActivatedRoute, 
-    private dialog: MatDialog, private router: Router) { }
+  constructor(protected programService: ProgramService, protected userService: UserService,
+    protected route: ActivatedRoute, 
+    private dialog: MatDialog, protected router: Router) {
+      super(programService, userService, router, route);
+     }
 
   ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.programService.getById(id).subscribe(
-      (result: Program) => {
-        this.program = result;
-      },
-      (error: HttpErrorResponse) => {
-        if(error.status === 401) {
-          localStorage.removeItem("token");
-          this.router.navigate(['login']);
-        }
-        this.message = error.error.message;
-      }
-    );
-  }
-
-  loadCourse(id: number): void {
-    this.programService.getById(id).subscribe(
-      (result: Program) => {
-        this.program = result;
-      },
-      (error: HttpErrorResponse) => {
-        if(error.status === 401) {
-          localStorage.removeItem("token");
-          this.router.navigate(['login']);
-        }
-        this.message = error.error.message;
-      }
-    );
+    this.getElem();
   }
 
   delete(id: number, name: string) {
