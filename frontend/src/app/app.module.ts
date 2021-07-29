6eb387f66d1822cfd0eb4e914a975ebc98e6db5e
xpax <55586470+xpakx@users.noreply.github.com>
2021-07-29 12:52:41 +0200

    chore(frontend): delete unneccesary imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,18 +7,12 @@ import { AppComponent } from './app.component';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSelectModule } from '@angular/material/select';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -217,18 +211,12 @@ export function tokenGetter() {
     FlexLayoutModule,
     FormsModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule,
     MatToolbarModule,
     MatMenuModule,
     MatIconModule,
     MatButtonModule,
-    MatTableModule,
-    MatDividerModule,
-    MatProgressSpinnerModule,
     MatInputModule,
     MatCardModule,
-    MatSlideToggleModule,
-    MatSelectModule,
     MatDialogModule,
     MatCheckboxModule,
     MatListModule,
